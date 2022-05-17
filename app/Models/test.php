261f461b6142f8eef3a88cf117e69773e1f60cbf
tariqul islam <tariqul.cse027@gmail.com>
2022-05-18 00:07:11 +0600

    continue

---
+++ app/Models/test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class test extends Model
+{
+    use HasFactory;
+    protected $table = 'test';
+    protected $fillable = [
+    	'name',
+    	'description'
+    ]
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\TestController;
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('students', [StudentController::class, 'index']);
+Route::post('/add-student', [StudentController::class, 'store']);
+Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+Route::put('update-student/{id}', [StudentController::class, 'update']);
+Route::delete('delete-student/{id}', [StudentController::class, 'destroy']);
